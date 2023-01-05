e7cf59c859d5c1a63bbee347176c7e42edd36b38
Smiley <jcsmileyjr@gmail.com>
2023-01-05 04:15:33 -0600

    WIP

--- react-episode-listing/src/App.js
+++ react-episode-listing/src/App.js
@@ -1,5 +1,7 @@
 import "./App.css";
 import CompressFM from "./assets/podcast-cover.png";
+import Data from './development-data/data.json';
+import React, {useState} from 'react'
 
 /**
  * Phase 1: build out center UI without todos - DONE
@@ -7,12 +9,18 @@ import CompressFM from "./assets/podcast-cover.png";
  * Phase 3: Todo functionality
  */
 function App() {
+  const [todos, setTodos] = useState([Data]);
   return (
     <div className="App">
       <img src={CompressFM} alt="podcast cover" className="image--style" />
 
       <div className="listing--container">
         <p>Listen to all the COMPRESSED.FM episodes</p>
+        {
+          todos.map(todo => (
+            <p>|| {todo.content}</p>
+          ))
+        }
       </div>
     </div>
   );
