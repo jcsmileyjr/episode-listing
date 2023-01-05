045f1d6a45c2402591c0864ca778e6d5208d3b98
Smiley <jcsmileyjr@gmail.com>
2023-01-04 19:56:13 -0600

    Refactor styling and structure for the basic UI format

--- react-episode-listing/src/App.js
+++ react-episode-listing/src/App.js
@@ -1,10 +1,20 @@
-
 import './App.css';
+import CompressFM from './assets/podcast-cover.png';
 
+/**
+ * Phase 1: build out center UI without todos
+ * Phase 2: build out the todos
+ * Phase 3: Todo functionality
+ */
 function App() {
   return (
     <div className="App">
-
+      
+        <img src={CompressFM} alt="podcast cover" className="image--style" />
+      
+      <div className="listing--container">
+        <p>Listen to all the COMPRESSED.FM episodes</p>
+      </div>
     </div>
   );
 }
