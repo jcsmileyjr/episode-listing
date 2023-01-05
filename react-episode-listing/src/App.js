d38cb2d21fa232d36c1f7d25bdd8f8e15e068000
Smiley <jcsmileyjr@gmail.com>
2023-01-05 04:22:36 -0600

    Feature: Initial todo data formating

--- react-episode-listing/src/App.js
+++ react-episode-listing/src/App.js
@@ -9,18 +9,22 @@ import React, {useState} from 'react'
  * Phase 3: Todo functionality
  */
 function App() {
-  const [todos, setTodos] = useState([Data]);
+  const [todos, setTodos] = useState(Data);
+  console.log(todos);
   return (
     <div className="App">
       <img src={CompressFM} alt="podcast cover" className="image--style" />
 
       <div className="listing--container">
-        <p>Listen to all the COMPRESSED.FM episodes</p>
-        {
-          todos.map(todo => (
-            <p>|| {todo.content}</p>
-          ))
-        }
+        <div className="listing__emptyDiv--container"></div>
+        <div className="listing__content--container">
+          <p>Listen to all the COMPRESSED.FM episodes</p>
+          {
+            todos.map(todo => (
+              <p>|| {todo.content}</p>
+            ))
+          }
+        </div>
       </div>
     </div>
   );
