4d39287f89189df0716c2b4539111d4ebbb59a0d
Smiley <jcsmileyjr@gmail.com>
2023-01-05 04:04:44 -0600

    Refactor fonts, responsive design, and etc.

--- react-episode-listing/src/App.js
+++ react-episode-listing/src/App.js
@@ -1,17 +1,16 @@
-import './App.css';
-import CompressFM from './assets/podcast-cover.png';
+import "./App.css";
+import CompressFM from "./assets/podcast-cover.png";
 
 /**
- * Phase 1: build out center UI without todos
+ * Phase 1: build out center UI without todos - DONE
  * Phase 2: build out the todos
  * Phase 3: Todo functionality
  */
 function App() {
   return (
     <div className="App">
-      
-        <img src={CompressFM} alt="podcast cover" className="image--style" />
-      
+      <img src={CompressFM} alt="podcast cover" className="image--style" />
+
       <div className="listing--container">
         <p>Listen to all the COMPRESSED.FM episodes</p>
       </div>
