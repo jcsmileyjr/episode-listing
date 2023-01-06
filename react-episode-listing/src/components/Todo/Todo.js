e8c1f44656c9fcbc9d7613cffae0d5dcd027ff4c
Smiley <jcsmileyjr@gmail.com>
2023-01-06 13:35:25 -0600

    Refactor styling for the TODO

--- react-episode-listing/src/components/Todo/Todo.js
+++ react-episode-listing/src/components/Todo/Todo.js
@@ -1,11 +1,14 @@
 import "./todo.css";
 
-const Todo = ({data, numbering}) => {
-    return (
-        <div className="todo--container">
-            <p>{numbering + 1} || {data}</p>
-        </div>
-    )
-}
+const Todo = ({ data, numbering }) => {
+  return (
+    <div className="todo--container">
+      <input id={`check-${numbering}`} type="checkbox" className="todo__checkbox--style" />
+      <label for={`check-${numbering}`}>
+        {numbering + 1} || {data}
+      </label>
+    </div>
+  );
+};
 
 export default Todo;
