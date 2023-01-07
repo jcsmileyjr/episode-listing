08827606ec9eb96fe4ca60480d33cac6fdd5a718
Smiley <jcsmileyjr@gmail.com>
2023-01-06 19:15:41 -0600

    Bug: Fix improper "for" tag on label

--- react-episode-listing/src/components/Todo/Todo.js
+++ react-episode-listing/src/components/Todo/Todo.js
@@ -4,7 +4,7 @@ const Todo = ({ data, numbering, done }) => {
   return (
     <div className="todo--container">
       <input id={`check-${numbering}`} type="checkbox" className="todo__checkbox--style" />
-      <label for={`check-${numbering}`} className={`${done?"scratch-off":""}`}>
+      <label htmlFor={`check-${numbering}`} className={`${done?"scratch-off":""}`}>
         {numbering + 1} || {data}
       </label>
     </div>
