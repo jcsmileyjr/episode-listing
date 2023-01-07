c277625581a3e64e4b856f7f39e9c8dbb0204067
Smiley <jcsmileyjr@gmail.com>
2023-01-06 19:36:35 -0600

    Feature: Functionality to scratch off all todos

--- react-episode-listing/src/App.js
+++ react-episode-listing/src/App.js
@@ -4,13 +4,9 @@ import Data from './development-data/data.json';
 import React, {useState} from 'react'
 import Todo from "./components/Todo/Todo";
 
-/**
- * Phase 1: build out center UI without todos - DONE
- * Phase 2: build out the todos
- * Phase 3: Todo functionality
- */
 function App() {
   const [todos, setTodos] = useState(Data);
+  const [allChecked, setAllChecked] = useState(false);
 
   /**
    * Function to update the scratch off a todo when the checkbox is checked
@@ -29,6 +25,26 @@ function App() {
       }
     }))
   }
+
+  /**
+   * function to scratch off all todos
+   */
+  const checkall = () => {
+    if(!allChecked){
+      setTodos(todos.map((todo) => {
+        todo.done = true;
+        return todo;
+      }))
+      setAllChecked(true);
+    }else{
+      setTodos(todos.map((todo) => {
+        todo.done = false;
+        return todo;
+      }))
+      setAllChecked(false);
+    }
+  }
+
   return (
     <div className="App">
       <img src={CompressFM} alt="podcast cover" className="image--style" />
@@ -37,6 +53,10 @@ function App() {
         <div className="listing__emptyDiv--container"></div>
         <div className="listing__content--container">
           <p>Listen to all the COMPRESSED.FM episodes</p>
+          <label htmlFor="checkall" className="listing__checkall--conatiner">
+            <input type="checkbox" className="listing__checkall--style" onChange={() => checkall()} />
+            Check all
+          </label>
           {
             todos.map((todo, index) => (
               <Todo scratch={() => scratchOffTodo(todo.id)}  data={todo} numbering={index} key={index} />
--- react-episode-listing/src/components/Todo/Todo.js
+++ react-episode-listing/src/components/Todo/Todo.js
@@ -3,8 +3,16 @@ import "./todo.css";
 const Todo = ({ data, numbering, scratch }) => {
   return (
     <div className="todo--container">
-      <input id={`check-${numbering}`} type="checkbox" className="todo__checkbox--style" onChange={scratch} />
-      <label htmlFor={`check-${numbering}`} className={`${data.done?"scratch-off":""}`}>
+      <input
+        id={`check-${numbering}`}
+        type="checkbox"
+        className="todo__checkbox--style"
+        onChange={scratch}
+      />
+      <label
+        htmlFor={`check-${numbering}`}
+        className={`${data.done ? "scratch-off" : ""}`}
+      >
         {numbering + 1} || {data.content}
       </label>
     </div>
