4df82aa21dc746747b5af668e99f0c304fee40e4
Smiley <jcsmileyjr@gmail.com>
2023-01-11 13:01:38 -0600

    Vanilla JS iteration: Documentation of JS todo list creation

--- vanilla-javascript-episode-listing/index.js
+++ vanilla-javascript-episode-listing/index.js
@@ -8,11 +8,10 @@ let todoList = document.getElementById("todoList")
  * 3. Use JS to display those inputs and label using data from the array
  */
 document.addEventListener("DOMContentLoaded", () => {
-    console.log("Hello World!");
-    
     fetchData();
 });
 
+// Function calling an static file to dynamic create a list of TODOS
 function fetchData() {
     fetch('../react-episode-listing/src/development-data/data.json')
     .then((res) => res.json())
@@ -23,26 +22,33 @@ function fetchData() {
     })
 }
 
+/**
+ * Function that dynamically create a list of todos appended to the #todoList div from
+ * the data gather in the fetchData().
+ * @param {array} todos - Todo objects
+ */
 const displayTodos = (todos) => {
     todos.forEach((todo) => {
-        
+        // Create the checkbox input
         let todoCheckbox = document.createElement('input');
         todoCheckbox.type = 'checkbox';
         todoCheckbox.id = `check- ${todo.id}`;
         todoCheckbox.class = "todo__checkbox--style";
         todoCheckbox.checked = todo.done;
 
+        // Create the label for the above input field
         let todoLabel = document.createElement('label');
         todoLabel.for = `check- ${todo.id}`;
         todoLabel.className = `${todo.done ? "scratch-off" : ""}`;
         todoLabel.textContent = `${todo.id + 1} || ${todo.content}`;
 
+        // Create the container holding the above input and label
         let todoContainer = document.createElement('div');
         todoContainer.className = "todoContainer--style";
 
+        // Append the container of input and label to the #todoList div
         todoContainer.append(todoCheckbox);
         todoContainer.append(todoLabel);
         todoList.append(todoContainer)
     })
-
 }
